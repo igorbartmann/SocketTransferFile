09dbecf617461b71fe1c202196a05fd5b40c83dc
Igor Bartmann <ibartmann@gvdasa.com.br>
2023-11-13 12:59:11 -0300

    Tarefa da cadeira de Redes envolvendo transferência de arquivos entre cliente e servidor utilizando socket.

---
+++ Client/Application/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Application
+{
+    public class Client
+    {
+        public static async Task Main(string[] args)
+        {
+            #region Constants
+            const int SERVER_PORT = 11000;
+            const string RECEIVED_FILES_DIRECTORY_PATH = @"C:\Users\ibartmann\OneDrive - GVDASA\Área de Trabalho\Faculdade\Redes de Computadores\Socket_Communication\Client\Application\ReceivedFiles";
+            const string GET_FILES_LIST_COMMAND = "Get_Files";
+            const string GET_FILE_ID_COMMAND = "Get_File_";
+            #endregion
+
+            // Inicializando IpEndPoint (par endereço e porta).
+            var ipEndPoint = await GetIpEndPoint(SERVER_PORT);
+
+            // Executa até que seja cancelado pelo usuário.
+            bool run = true;
+            while (run)
+            {
+                // Cria o socket.
+                using (var socket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    // Conecta com o servidor.
+                    await socket.ConnectAsync(ipEndPoint);
+
+                    // Imprime mensagem alertando sobre a conexão ter sido estabelecida.
+                    Console.WriteLine("Socket connection established...");
+
+                    // Imprime menu.
+                    Console.WriteLine(@$"
+                        ***************************************************
+                                                MENU
+                        [1] - Obter Arquivos disponíveis.
+                        [2] - Obter Arquivo.
+                        [3] - Sair.
+                        ***************************************************");
+
+                    // Solicita opção do menu desejada.
+                    Console.Write("Digite a opção desejada:");
+                    var optionString = Console.ReadLine();
+                    if (Int32.TryParse(optionString, out int option) && option >= 1 && option <=3)
+                    {
+                        // Trata opção selecionada.
+                        switch (option)
+                        {
+                            case 1: // Solicita lista de arquivos para o servidor.
+                                var commandFileListBytes = Encoding.UTF8.GetBytes(GET_FILES_LIST_COMMAND);
+                                await socket.SendAsync(commandFileListBytes, SocketFlags.None);
+                                Console.WriteLine("Socket client sent request: \"GET_FILES_LIST_COMMAND\"");
+
+                                var responseBytes = new byte[1024];
+                                var responseLength = await socket.ReceiveAsync(responseBytes, SocketFlags.None);
+                                var response = Encoding.UTF8.GetString(responseBytes, 0, responseLength);
+
+                                Console.WriteLine($"Files available on the server: \n{response}");
+                                break;
+                            case 2: // Solicita arquivo específico para o servidor.
+                                Console.Write("Digite o código identificador (Id) do arquivo a ser obtido no servidor: ");
+
+                                var fileIdString = Console.ReadLine();
+                                if (Int32.TryParse(fileIdString, out int fileId))
+                                {
+                                    var commandFileIdString = GET_FILE_ID_COMMAND + fileId;
+                                    var commandFileIdBytes = Encoding.UTF8.GetBytes(commandFileIdString);
+                                    await socket.SendAsync(commandFileIdBytes, SocketFlags.None);
+                                    Console.WriteLine("Socket client sent request: \"GET_FILE_ID_COMMAND\"");
+
+                                    var responseFileBytes = new byte[1024 * 50000];
+                                    var responseFileLength = socket.Receive(responseFileBytes, SocketFlags.None);
+                                    if (responseFileLength > 0)
+                                    {
+                                        var fileNameLength = BitConverter.ToInt16(responseFileBytes, 0);
+                                        var fileName = Encoding.UTF8.GetString(responseFileBytes, 4, fileNameLength);
+
+                                        var filePath = Path.Combine(RECEIVED_FILES_DIRECTORY_PATH, fileName);
+                                        BinaryWriter binaryWriter = new BinaryWriter(File.Open(filePath, FileMode.Create));
+                                        binaryWriter.Write(responseFileBytes, 4 + fileNameLength, responseFileLength - 4 - fileNameLength);
+
+                                        while (responseFileLength > 0)
+                                        {
+                                            responseFileLength = socket.Receive(responseFileBytes, responseFileBytes.Length, SocketFlags.None);
+                                            if (responseFileLength == 0)
+                                            {
+                                                binaryWriter.Close();
+                                            }
+                                            else
+                                            {
+                                                binaryWriter.Write(responseFileBytes, 0, responseFileLength);
+                                            }
+                                        }
+
+                                        Console.WriteLine($"Socket client received the file as response and stored it in {filePath}!");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"Erro ao consultar o arquivo!");
+                                    }
+                                }
+                                else 
+                                {
+                                    Console.WriteLine("Erro, id inválido (deve ser digitado um número)!");
+                                }
+                                break;
+                            case 3: // Encerra a execução.
+                                run = false;
+                                break;
+                        }
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Erro, opção inválida!");
+                    }
+
+                    // Finaliza o socket cliente.
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+            }
+
+            // Imprime mensagem de encerramento da aplicação cliente.
+            Console.WriteLine("Client application finished!");
+        }
+
+        /// <summary>
+        /// Obter IPEndPoint.
+        /// </summary>
+        /// <param name="port">Número da porta ao qual o servidor estará atrelado.</param>
+        /// <returns>Objeto do tipo IPEndPoint.</returns>
+        private static async Task<IPEndPoint> GetIpEndPoint(int port)
+        {
+            IPHostEntry ipHostEntry = await Dns.GetHostEntryAsync(Dns.GetHostName());
+            IPAddress ipAddress = ipHostEntry.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+
+            return ipEndPoint;
+        }
+    }
+}
---
+++ Server/Application/Program.cs
@@ -0,0 +1,163 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Application
+{
+    public class Server
+    {
+        public static async Task Main(string[] args)
+        {
+            #region Constants
+            const int SERVER_PORT = 11000;
+            const int SERVER_PENDING_CONNECTIONS_QUEUE_LENGTH = 100;
+            const string FILES_DIRECTORY_PATH = @"C:\Users\ibartmann\OneDrive - GVDASA\Área de Trabalho\Faculdade\Redes de Computadores\Socket_Communication\Server\Application\Files";
+            const string GET_FILES_LIST_COMMAND = "Get_Files";
+            const string GET_FILE_ID_COMMAND = "Get_File_";
+            #endregion
+
+            // Inicializando lista dos arquivos armazenados no servidor.
+            var fileList = GetFilesStored(FILES_DIRECTORY_PATH);
+
+            // Inicializando IpEndPoint (par endereço e porta).
+            var ipEndPoint = await GetIpEndPoint(SERVER_PORT);
+
+            // Criando socket.
+            using (var serverSocket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+            {
+                // Vinculando socket criado com o IpEndPoint (par endereço e porta).
+                serverSocket.Bind(ipEndPoint);
+
+                // Definindo tamanho máximo da fila de espera.
+                serverSocket.Listen(SERVER_PENDING_CONNECTIONS_QUEUE_LENGTH);
+
+                // Imprime mensagem alertando sobre o socket servidor estar disponível.
+                Console.WriteLine("Socket server available...");
+
+                // Socket server roda até que a operação seja cancelada.
+                while (true)
+                {
+                    // Aguarda conexão com cliente.
+                    var clientSocket = await serverSocket.AcceptAsync();
+                    
+                    // Cria buffer para armazenar bytes recebidos.
+                    var receivedRequestBytes = new byte[1024];
+
+                    // Aguarda recebimento de dados da conexão via socket com o cliente.
+                    var receivedRequestLength = await clientSocket.ReceiveAsync(receivedRequestBytes, SocketFlags.None);
+
+                    // Converte os bytes recebidos para string.
+                    var receivedRequest = Encoding.UTF8.GetString(receivedRequestBytes, 0, receivedRequestLength);
+
+                    // Imprime a mensagem recebida do cliente.
+                    Console.WriteLine($"Socket server received the request: \"{receivedRequest}\"");
+
+                    // Trata o comando recebido.
+                    byte[]? response = null;
+                    if (receivedRequest.Contains(GET_FILES_LIST_COMMAND, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        var stringBuilder = new StringBuilder();
+                        foreach (var file in fileList)
+                        {
+                            stringBuilder.AppendLine($"FileId: ({file.Id}) - FileName: \"{file.Name}\"\n");
+                        }
+
+                        response = Encoding.UTF8.GetBytes(stringBuilder.ToString());
+                    }
+                    else if (receivedRequest.Contains(GET_FILE_ID_COMMAND, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        var fileIdString = receivedRequest.Replace(GET_FILE_ID_COMMAND, string.Empty);
+                        if (Int32.TryParse(fileIdString, out int fileId))
+                        {
+                            var archive = fileList.FirstOrDefault(file => file.Id == fileId);
+                            if (archive is not null)
+                            {
+                                var fileNameBytes = Encoding.UTF8.GetBytes(archive.Name);
+                                var fileNameBytesLengthInBytes = BitConverter.GetBytes(fileNameBytes.Length);
+                                var fileBytes = File.ReadAllBytes(archive.Path);
+                                
+                                response = new byte[4 + fileNameBytes.Length + fileBytes.Length];
+                                fileNameBytesLengthInBytes.CopyTo(response, 0);
+                                fileNameBytes.CopyTo(response, 4);
+                                fileBytes.CopyTo(response, 4 + fileNameBytes.Length);
+                            }
+                            else 
+                            {
+                                response = Encoding.UTF8.GetBytes(string.Empty);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        response = Encoding.UTF8.GetBytes("Command not recognized!");
+                    }
+
+                    if (response is not null)
+                    {
+                        // Enviado resposta ao comando recebido.
+                        await clientSocket.SendAsync(response, SocketFlags.None);
+                            
+                        // Imprime mensagem alertando que o socket servidor responder.
+                        Console.WriteLine("Socket server sent response!");
+                    }
+                    else 
+                    {
+                        throw new ApplicationException("An error has ocurred!");
+                    }                    
+
+                    // Finaliza o socket de comunicação com o cliente.
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Carrega os arquivos armazenados no servidor.
+        /// </summary>
+        /// <param name="path">Caminho do diretório dos arquivos.</param>
+        /// <returns>Lista com os arquivos armazenados no servidor.</returns>
+        private static IList<Archive> GetFilesStored(string path)
+        {
+            // Inicializando lista dos arquivos armazenados no servidor.
+            var filesNameWithPath = Directory.GetFiles(path);
+
+            int currentFileId = 0;
+            var fileList = new List<Archive>();
+            foreach (var fileNameWithPath in filesNameWithPath)
+            {
+                fileList.Add(new Archive(++currentFileId, Path.GetFileName(fileNameWithPath), fileNameWithPath));
+            }
+
+            return fileList;
+        }
+
+        /// <summary>
+        /// Obter IPEndPoint.
+        /// </summary>
+        /// <param name="port">Número da porta ao qual o servidor estará atrelado.</param>
+        /// <returns>Objeto do tipo IPEndPoint.</returns>
+        private static async Task<IPEndPoint> GetIpEndPoint(int port)
+        {
+            IPHostEntry ipHostEntry = await Dns.GetHostEntryAsync(Dns.GetHostName());
+            IPAddress ipAddress = ipHostEntry.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+
+            return ipEndPoint;
+        }
+    }
+
+    public class Archive
+    {
+        public Archive(int id, string name, string path)
+        {
+            Id = id;
+            Name = name;
+            Path = path;
+        }
+
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public string Path {get;set;}
+    }
+}
